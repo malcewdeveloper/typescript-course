1dc8f7a0d77f85b24b91db91c35605dd65a2ee8e
Maksim <malcewdeveloper@yandex.ru>
2022-12-08 16:23:11 +0300

    typescript/interfaces

---
+++ client/src/typescript/3_typescript.ts
@@ -0,0 +1,52 @@
+// Interfaces
+interface Owner {
+    name: string,
+}
+
+interface Dog {
+    weight: number;
+    readonly color: string;
+    name: string;
+    bark: () => void;
+    owner: Owner;
+    awards?: Awards[];
+}
+
+interface Awards {
+    price: number
+}
+
+interface frenchBulldog extends Dog {
+    color: 'fawn' | 'black' | 'brown';
+    snorihgVolume: number;
+}
+
+const owner: Owner = {
+    name: 'Maksim'
+}
+
+const frenchBulldog: frenchBulldog = {
+    weight: 12,
+    color: 'fawn',
+    name: 'Richard',
+    bark() {
+        console.log('bark');
+    },
+    owner: owner,
+    awards: [{price: 10000}],
+    snorihgVolume: 10
+}
+
+const ownerDogie = {
+    name: 'Daria'
+}
+
+const corgi: Dog = {
+    weight: 10,
+    color: 'blue', 
+    name: 'dogie',
+    bark() {
+        console.log('dogie');
+    },
+    owner: ownerDogie
+}
